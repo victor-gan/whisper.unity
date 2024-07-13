117e3e0a13c6eb87beda24c24d68decb3a5e6036
victor-gan <victorgan9221@gmail.com>
2024-07-14 11:09:42 +1200

    Change LLM output to text box

--- Assets/Scripts/AI/LLMManager.cs
+++ Assets/Scripts/AI/LLMManager.cs
@@ -2,21 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using LLMUnity;
+using UnityEngine.UI;
 
 public class LLMManager : MonoBehaviour
 {
     public LLMCharacter llmCharacter;
     public static LLMManager Instance;
+    public Text outputText;
 
     void Awake(){
         Instance = this;
     }
 
     void HandleReply(string reply){
-    Debug.Log(reply);
+        outputText.text = reply;
     }
 
     public void PromptAI(string message){
-    _ = llmCharacter.Chat(message, HandleReply);
+        _ = llmCharacter.Chat(message, HandleReply);
     }
 }
